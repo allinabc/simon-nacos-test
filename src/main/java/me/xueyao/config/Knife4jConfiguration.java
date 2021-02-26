37a99f712ee08d3eaf01ea0b204a4701ae4ff1b0
simonxue <xueyao.me@gmail.com>
2021-02-27 01:50:44 +0800

    feat:添加微服务Swagger(Knife4j)

---
+++ src/main/java/me/xueyao/config/Knife4jConfiguration.java
@@ -0,0 +1,39 @@
+package me.xueyao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @author Simon.Xue
+ * @date 2021/2/27 1:32 上午
+ **/
+@Configuration
+@EnableSwagger2WebMvc
+public class Knife4jConfiguration {
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket=new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        //.title("swagger-bootstrap-ui-demo RESTful APIs")
+                        .description("# Simon Nacos APIs")
+                        .termsOfServiceUrl("http://blog.xueyao.me/")
+                        .contact("xueyao.me@gmail.com")
+                        .version("1.0")
+                        .build())
+                //分组名称
+                .groupName("2.X版本")
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("me.xueyao.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+}
